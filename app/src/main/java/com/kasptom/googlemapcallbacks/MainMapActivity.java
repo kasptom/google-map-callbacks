2a6c36bdc6727d472e95a72f1f1b838feaced131
Tomasz Kasprzyk <kasptom@gmail.com>
2016-10-15 12:07:41 +0200

    Initial commit   * Add Timber for logging   * Add Google Play services for map   * Add API KEY entry to google_maps_api.xml   * Add google_maps_api.xml to .gitignore

---
+++ app/src/main/java/com/kasptom/googlemapcallbacks/MainMapActivity.java
@@ -0,0 +1,37 @@
+package com.kasptom.googlemapcallbacks;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import timber.log.Timber;
+
+
+public class MainMapActivity extends AppCompatActivity implements OnMapReadyCallback
+{
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_map);
+
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        LatLng krakow = new LatLng(50.06465, 19.94497);
+        map = googleMap;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(krakow, 13.0f));
+        Timber.d("onMapReady");
+    }
+}
---
+++ app/src/main/java/com/kasptom/googlemapcallbacks/MapApp.java
@@ -0,0 +1,15 @@
+package com.kasptom.googlemapcallbacks;
+
+import android.app.Application;
+import timber.log.Timber;
+
+
+public class MapApp extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+}
